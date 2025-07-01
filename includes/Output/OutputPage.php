573620ce90d070b1f8cce35a8953a7b6a2d9b992
Nano <hyeunano@gmail.com>
2025-07-02 08:25:41 +0900

    chore: remove all unnecessary classes from body element

--- includes/Output/OutputPage.php
+++ includes/Output/OutputPage.php
@@ -3618,7 +3618,7 @@ class OutputPage extends ContextSource {
 		// Then run the hook, the recommended way of adding body attributes now
 		$this->getHookRunner()->onOutputPageBodyAttributes( $this, $sk, $bodyAttrs );
 
-		$pieces[] = Html::openElement( 'body', $bodyAttrs );
+		$pieces[] = Html::openElement( 'body' );
 
 		return self::combineWrappedStrings( $pieces );
 	}
